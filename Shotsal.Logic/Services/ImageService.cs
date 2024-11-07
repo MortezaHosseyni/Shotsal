772a5b782a7a182b2d06103f8478d71dd82d467d
Morteza Hosseini <morteza_hosseini_dev@outlook.com>
2024-11-07 12:00:58 +0330

    Start & Configurations Create Layers: Api, Logic and Data Configure Api layer Create Image service in Logic layer Create Save and Get methods in Image service Install Redis in Logic layer

---
+++ Shotsal.Logic/Services/ImageService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using StackExchange.Redis;
+
+namespace Shotsal.Logic.Services
+{
+    public interface IImageService
+    {
+        Task<string> SaveImageAsync(IFormFile imageFile, TimeSpan expiration);
+        Task<byte[]> GetImageAsync(string key);
+    }
+
+    public class ImageService(IConnectionMultiplexer redis) : IImageService
+    {
+        public async Task<string> SaveImageAsync(IFormFile imageFile, TimeSpan expiration)
+        {
+            var db = redis.GetDatabase();
+            var key = Guid.NewGuid().ToString();
+
+            using var memoryStream = new MemoryStream();
+            await imageFile.CopyToAsync(memoryStream);
+            var imageBytes = memoryStream.ToArray();
+
+            await db.StringSetAsync(key, imageBytes, expiration);
+            return key;
+        }
+
+        public async Task<byte[]> GetImageAsync(string key)
+        {
+            var db = redis.GetDatabase();
+            return (await db.StringGetAsync(key))!;
+        }
+    }
+}
